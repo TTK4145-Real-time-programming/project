9e17d7bbe12fbeba363ce25e22876abef108a21b
Mathias Otnes <Mathias.otnes@gmail.com>
2024-01-24 21:11:01 +0100

    1: Hardware driver linked

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-mod fsm;
-mod control;
+/* Libraries */
+use driver_rust::elevio;
 
+/* Modules */
+mod control;
+mod fsm;
+
+/* Main program */
 fn main() {
     fsm::fsm();
     control::control()
